9911333cdb13709c6e89b22a21134abb53639cc9
“Francesca <dascanio.francesca@gmail.com>
2023-01-11 16:26:54 +0100

    Completato esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,12 @@ L'utente clicca su un bottone che genererà una griglia di gioco quadrata.      
 Ogni cella ha un numero progressivo, da 1 a 100.                                    OK
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.                             OK
 
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. OK
 
 */
 
 // funzione crea N celle con numero progressivo
-function createCells (min, max, cell, container) {
+function createCells (min, max, container) {
 
         //Crea N celle e inseriscile in un contenitore
         for (let i = min; i <= max; i++ ) {
@@ -22,8 +21,24 @@ function createCells (min, max, cell, container) {
             cell.classList.add('stile-cella');
             cell.classList.add('riga-dieci');
             cell.innerHTML = i;
-            console.log('La cella ' + i+ ' corrisponde a', cell);
             container.append(cell);
+
+
+            // Al click di ogni cella 
+            cell.addEventListener ('click',
+            
+                function () {
+
+                    if (this.classList.contains('clicked')) {
+                        this.classList.remove('clicked');
+                    }
+                    else {
+                        this.classList.add('clicked');
+                        console.log('Il numero della cella cliccata è: ', i);
+                    }
+                }
+            
+            )
         }
     
 
@@ -35,8 +50,6 @@ function createCells (min, max, cell, container) {
 const buttonPlay = document.getElementById('button-play');
 console.log(buttonPlay);
 
-// E' scorretto creare una variabile globale qui?
-let myCell; 
 
 buttonPlay.addEventListener ('click',
 
@@ -45,20 +58,8 @@ buttonPlay.addEventListener ('click',
         const myGrid = document.getElementById('grid');
         console.log(myGrid);
 
-        myCell = createCells (1, 100, myCell, myGrid);
-        console.log(myCell);
-
-        // // Crea 100 celle e inseriscile in div grid
-        // for (let i = 1; i <= 100; i++ ) {
-
-        //     // Crea nuova cella
-        //     const myCell = document.createElement('div');
-
-        //     // // Attribuisci al mio nuovo div cella la classe che ne determina le dimensioni --> è giusto ??
-        //     myCell.classList.add('one-hundred');
-        //     console.log('La cella ' + i+ ' corrisponde a', myCell);
-        //     myGrid.append(myCell);
-        // }
+        allCells = createCells (1, 100, myGrid);
+        console.log(allCells);
 
     }
     // Per fermare il click: posso aggiungere solo 1 volta la griglia --> soluzione temporanea
